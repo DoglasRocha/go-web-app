50d269a1e2edb997c2ba0dfac51b4b55e878b50b
Doglas Rocha <79492860+DoglasRocha@users.noreply.github.com>
2022-06-28 00:16:08 -0300

    modularize the project

--- main.go
+++ main.go
@@ -1,29 +1,10 @@
 package main
 
 import (
-	"database/sql"
 	"html/template"
+	"loja/models"
 	"net/http"
-
-	_ "github.com/lib/pq"
 )
-
-func conectaComBancoDeDados() *sql.DB {
-	conexao := "user=postgres dbname=doglas_loja password=12345678 host=localhost sslmode=disable"
-	db, err := sql.Open("postgres", conexao)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	return db
-}
-
-type Produto struct {
-	Id              int
-	Nome, Descricao string
-	Preco           float64
-	Quantidade      int
-}
 
 var templates = template.Must(template.ParseGlob("templates/*.html"))
 
@@ -33,26 +14,8 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	db := conectaComBancoDeDados()
-	selectDeTodosOsProdutos, err := db.Query("SELECT * FROM produtos")
 
-	if err != nil {
-		panic(err.Error())
-	}
+	todosOsProdutos := produtos.BuscaTodosOsProdutos()
 
-	p := Produto{}
-	produtos := []Produto{}
-
-	for selectDeTodosOsProdutos.Next() {
-		err = selectDeTodosOsProdutos.Scan(&p.Id, &p.Nome, &p.Descricao, &p.Preco, &p.Quantidade)
-
-		if err != nil {
-			panic(err.Error())
-		}
-
-		produtos = append(produtos, p)
-	}
-
-	templates.ExecuteTemplate(w, "Index", produtos)
-	defer db.Close()
+	templates.ExecuteTemplate(w, "Index", todosOsProdutos)
 }
---
+++ models/produtos.go
@@ -0,0 +1,35 @@
+package produtos
+
+import "loja/db"
+
+type Produto struct {
+	Id              int
+	Nome, Descricao string
+	Preco           float64
+	Quantidade      int
+}
+
+func BuscaTodosOsProdutos() []Produto {
+	db := db.ConectaComBancoDeDados()
+	selectDeTodosOsProdutos, err := db.Query("SELECT * FROM produtos")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	p := Produto{}
+	produtos := []Produto{}
+
+	for selectDeTodosOsProdutos.Next() {
+		err = selectDeTodosOsProdutos.Scan(&p.Id, &p.Nome, &p.Descricao, &p.Preco, &p.Quantidade)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		produtos = append(produtos, p)
+	}
+
+	defer db.Close()
+	return produtos
+}
